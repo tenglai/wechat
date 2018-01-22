b31c444d616257151c7235382aaeeb6dbf3ec30c
tenglai <13049322087@163.com>
2018-01-22 22:10:15 +0800

    add 自定义轮播图

--- school/pages/mine/mine.js
+++ school/pages/mine/mine.js
@@ -5,6 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    /*轮播图 配置*/
     imgUrls: [
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
@@ -13,8 +14,15 @@ Page({
     indicatorDots: true, // 是否显示面板指示点
     autoplay: true, // 是否自动切换
     interval: 5000, // 自动切换时间间隔
-    duration: 1000, // 滑动动画时长
-    circular: true // 是否采用衔接滑动
+    duration: 500, // 滑动动画时长
+    circular: true, // 是否采用衔接滑动
+    /*自定义轮播图 配置*/
+    slider: [
+      { id: '0', linkUrl: 'pages/index/index', picUrl: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg' },
+      { id: '0', linkUrl: 'pages/index/index', picUrl: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg' },
+      { id: '0', linkUrl: 'pages/index/index', picUrl: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg' }
+    ],
+    swiperCurrent: 0
   },
 
   /**
@@ -22,5 +30,19 @@ Page({
    */
   onLoad: function (options) {
   
+  },
+
+  //轮播图的切换事件  
+  swiperChange: function (e) {
+    //只要把切换后当前的index传给<swiper>组件的current属性即可  
+    this.setData({
+      swiperCurrent: e.detail.current
+    })
+  },
+  //点击指示点切换  
+  chuangEvent: function (e) {
+    this.setData({
+      swiperCurrent: e.currentTarget.id
+    })
   }
 })
